f5af029d2af5a791fce8c5d69a3c55e359f754a8
vasanwalker <36722736+vasanwalker@users.noreply.github.com>
2018-02-23 15:35:08 +0530

    Create numericstring.py

---
+++ beginner level/numericstring/numericstring.py
@@ -0,0 +1,14 @@
+v = '1242323'
+print(v.isnumeric())
+
+#v = '²3455'
+s = '\u00B23455'
+print(v.isnumeric())
+
+# v = '½'
+v = '\u00BD'
+print(v.isnumeric())
+
+v = '1242323'
+v='python12'
+print(v.isnumeric())
